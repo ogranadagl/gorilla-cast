77802bd40bd999fe80e7030218214aa9b4e96b93
Alejandro Benjumea <59701575+alejandro-benjumea-gorilla-logic@users.noreply.github.com>
2020-02-18 15:21:06 -0500

    [ALE-01] Add api module (#1)
    * Add 'ramda' and 'uuidd' dependencies
    * Add 'api' module

---
+++ src/api/utils.js
@@ -0,0 +1,38 @@
+import { curry } from 'ramda';
+
+/**
+ * Read key from local storage
+ * @param  {DOMString} keyName - A DOMString containing the name of the key you want to retrieve
+ * the value of.
+ * @param  {*} defaultValue - Default value
+ * @throws {SyntaxError}
+ * @return  {*} Parsed value retived by local storage or defaultValue if is a falsy value
+ */
+export function readKey(keyName, defaultValue) {
+  return JSON.parse(localStorage.getItem(keyName)) || defaultValue;
+}
+
+/**
+ * Curried funciton to add a key to the storage, or update that key's value if it already exists
+ * @param  {DOMString} keyName - A DOMString containing the name of the key you want to
+ * create/update.
+ * @param  {*} keyValue - Value you want to give the key you are creating/updating
+ * @return {*} keyValue
+ */
+export const saveKey = curry((keyName, keyValue) => {
+  localStorage.setItem(keyName, JSON.stringify(keyValue));
+  return keyValue;
+});
+
+/**
+ * Validate if value is empty and throws and error
+ * @param  {*} value - Value to validate
+ * @param  {String} key - Used in error message
+ * @throws {Error}
+ * @return {void}
+ */
+export function validateEmptyKey(value, key) {
+  if (value == null) {
+    throw new Error(`Missing "${key}" field`);
+  }
+}
