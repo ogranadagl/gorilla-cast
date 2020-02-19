be547e41a0cbfbdf60e8ab4fe7a894c6b2a2eb17
Alejandro Benjumea <alejandro.benjumea@gorillalogic.com>
2020-02-19 10:01:00 -0500

    Add 'search-lightbox' component

---
+++ src/components/search-lightbox/search-lightbox.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <v-text-field
+      :disabled="loading"
+      :loading="dialog"
+      @change="search"
+      label="Search"
+      placeholder="Search"
+      solo
+      v-model="term"
+    />
+
+    <v-dialog
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+      v-model="dialog"
+      width="600px"
+    >
+      <v-card>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="dialog = false">Close</v-btn>
+        </v-card-actions>
+        <v-card-title>
+          <span class="headline">Results</span>
+        </v-card-title>
+        <v-card-text>
+          <ul>
+            <li
+              v-for="result in results"
+              :key="result.trackId"
+              style="display: flex; padding: 20px;"
+            >
+              <img
+                :src="result.artworkUrl60"
+                style="width: 60px; display: block; height: 60px; margin-right: 20px;"
+              />
+              <div>
+                <h1>{{result.collectionName}}</h1>
+                <ul>
+                  <li v-for="genre in result.genres" :key="genre">
+                    <span>{{genre}}</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./styles.scss"></style>
