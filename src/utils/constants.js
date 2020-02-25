4987e8b8805d2c0dcbf697249facb9ffd36a4471
Alejandro Benjumea <alejandro.benjumea@gorillalogic.com>
2020-02-25 14:31:52 -0500

    Add 'MESSAGE_REMOVE_FAVORITE' definition

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@
 export const MESSAGE_PLAY_TRACK = 'play-track';
 export const MESSAGE_SEARCH_TRACK = 'search-track';
 export const MESSAGE_ADD_FAVORITE = 'add-favorite';
+export const MESSAGE_REMOVE_FAVORITE = 'remove-favorite';
 
 /* Constant to define the update rate */
 export const UPDATE_INTERVAL = 1000;
