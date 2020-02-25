05fa0a75f0a82839bc4b01b7a5016a85344702ce
Alejandro Benjumea <alejandro.benjumea@gorillalogic.com>
2020-02-25 14:30:07 -0500

    Add 'favorite-button' component

---
+++ src/components/favorite-button/favorite-button.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-tooltip left>
+    <template v-slot:activator="{ on }">
+      <div @click="handleToggleFavorite" v-on="on">
+        <v-icon v-if="track.meta.favoriteId" color="yellow">mdi-star</v-icon>
+        <v-icon v-else color="grey lighten-1">mdi-star-outline</v-icon>
+      </div>
+    </template>
+    <span>{{tooltipText}}</span>
+  </v-tooltip>
+</template>
+
+<script src="./script.js"></script>
