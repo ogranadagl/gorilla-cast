c2255e753c7aae6d0dcfa919a4422aae518a9a2a
Alejandro Benjumea <alejandro.benjumea@gorillalogic.com>
2020-02-26 11:47:00 -0500

    Rename 'main-layout' file and split files

---
+++ src/components/main-layout/main-layout.js
@@ -0,0 +1,12 @@
+import AppBar from '@/components/app-bar/app-bar.vue';
+import AppPlayer from '@/components/app-player/app-player.vue';
+import MenuDrawer from '@/components/menu-drawer/menu-drawer.vue';
+
+export default {
+  name: 'MainLayout',
+  components: {
+    AppBar,
+    AppPlayer,
+    MenuDrawer,
+  },
+};
---
+++ src/components/main-layout/main-layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app id="root-app">
+    <menu-drawer />
+
+    <app-bar />
+
+    <v-content>
+      <router-view />
+    </v-content>
+
+    <v-footer app>
+      <app-player />
+    </v-footer>
+  </v-app>
+</template>
+
+<script src="./main-layout.js"></script>
