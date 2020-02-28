e48f2925f3dd80b769341dc14eb6d76707d3f09f
Alejandro <you@example.com>
2020-02-28 11:23:36 -0500

    Refactor code styles in main-layout component

--- src/components/main-layout/main-layout.vue
+++ src/components/main-layout/main-layout.vue
@@ -1,13 +1,10 @@
 <template>
   <v-app id="root-app">
     <menu-drawer />
-
     <app-bar />
-
     <v-content>
       <router-view />
     </v-content>
-
     <v-footer app>
       <app-player />
     </v-footer>
